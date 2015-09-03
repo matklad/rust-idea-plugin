aa86d466e766584a5a1b628248559ed19cf29bf9
atsky <kurbatsky@gmail.com>
2015-09-03 19:57:23 +0200

    Register RustHighlighterFactory in plugin.xml

--- plugin/src/org/jetbrains/rust/RustLanguage.kt
+++ plugin/src/org/jetbrains/rust/RustLanguage.kt
@@ -7,15 +7,6 @@ import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
 import org.jetbrains.rust.highlight.RustHighlighter
 
 public class RustLanguage : Language("Rust", "text/rust") {
-
-    init {
-        SyntaxHighlighterFactory.LANGUAGE_FACTORY.addExplicitExtension(this, object : SingleLazyInstanceSyntaxHighlighterFactory() {
-            override fun createHighlighter(): SyntaxHighlighter {
-                return RustHighlighter()
-            }
-        })
-    }
-
     companion object {
         public val INSTANCE: RustLanguage = RustLanguage()
     }
