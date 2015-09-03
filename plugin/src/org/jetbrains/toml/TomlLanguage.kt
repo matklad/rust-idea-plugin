68a63964ec53a90f01f52d5006b13792fdb4ecd7
Aleksey Kladov <aleksey.kladov@gmail.com>
2015-09-03 18:55:36 +0300

    toml highlighter

--- plugin/src/org/jetbrains/toml/TomlLanguage.kt
+++ plugin/src/org/jetbrains/toml/TomlLanguage.kt
@@ -1,9 +1,22 @@
 package org.jetbrains.toml
 
 import com.intellij.lang.Language;
+import com.intellij.openapi.fileTypes.SingleLazyInstanceSyntaxHighlighterFactory
+import com.intellij.openapi.fileTypes.SyntaxHighlighter
+import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
+import org.jetbrains.toml.highlight.TomlHighlighter
 
 
-public class TomlLanguage: Language("TOML", "text/toml") {
+public class TomlLanguage : Language("TOML", "text/toml") {
+
+    init {
+        SyntaxHighlighterFactory.LANGUAGE_FACTORY.addExplicitExtension(this,
+                object : SingleLazyInstanceSyntaxHighlighterFactory() {
+                    override fun createHighlighter(): SyntaxHighlighter =
+                            TomlHighlighter()
+                })
+    }
+
     companion object {
         public val INSTANCE: TomlLanguage = TomlLanguage();
     }
