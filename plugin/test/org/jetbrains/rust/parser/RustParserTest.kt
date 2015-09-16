c3b3b9449ef3d8ef8852c8920070a151a63be7c9
Evgeny Kurbatsky <kurbatsky@gmail.com>
2015-09-16 23:21:28 +0200

    make test check for error absence only

--- plugin/test/org/jetbrains/rust/parser/RustParserTest.kt
+++ plugin/test/org/jetbrains/rust/parser/RustParserTest.kt
@@ -1,7 +1,12 @@
 package org.jetbrains.rust.parser
 
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiErrorElement
+import com.intellij.psi.PsiFile
 import com.intellij.testFramework.ParsingTestCase
 import org.jetbrains.rust.parser.grammar.RustParserDefinition
+import org.junit.Assert
 
 public class RustParserTest : ParsingTestCase("rust", "rs", RustParserDefinition()) {
 
@@ -13,6 +18,27 @@ public class RustParserTest : ParsingTestCase("rust", "rs", RustParserDefinition
         return "plugin/test/data"
     }
 
-    public fun testHello(): Unit = doTest(true)
+    public fun hasError(file: PsiFile): Boolean {
+        var hasErrors = false
+        file.accept(object : PsiElementVisitor() {
+            override fun visitElement(element: PsiElement?) {
+                if (element is PsiErrorElement) {
+                    hasErrors = true
+                    return
+                }
+                element!!.acceptChildren(this)
+            }
+        })
+        return hasErrors
+    }
+
+    fun doTest() {
+        val name = this.getTestName(true);
+        val text = this.loadFile(name + ".rs")
+        val psi = this.createPsiFile(name, text)
+        Assert.assertFalse(hasError(psi))
+    }
+
+    public fun testHello(): Unit = doTest()
 
 }
